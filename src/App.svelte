3719ec89e0ad1a7b9c2d3756b4e9fcebf32f9da9
Sergio Delgado <boldu@hotmail.com>
2021-03-24 16:00:38 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = "pepe"
+	let age = 45
+
+	function incrementAge(){
+		age += 1
+	}
+</script>
+
+<style>
+	h1 {
+		color: darkblue;
+	}
+</style>
+
+<h1>Hello {name}! you are {age}!</h1>
+<button on:click="{incrementAge}" >maaas!!!!</button>
